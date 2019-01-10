82f66cf9bfc9417e61d4071a876485db656fada1
legendstrange <legendstrange1992@gmail.com>
2019-01-10 22:57:33 +0700

    change php

--- layout/modal1.php
+++ layout/modal1.php
@@ -14,16 +14,8 @@
 								<div class="wrap-slick3-dots"></div>
 								<div class="wrap-slick3-arrows flex-sb-m flex-w"></div>
 
-								<div class="slick3 gallery-lb">
-									<div class="item-slick3" data-thumb="images/product-detail-01.jpg">
-										<div class="wrap-pic-w pos-relative">
-											<img src="images/product-detail-01.jpg" alt="IMG-PRODUCT">
-
-											<a class="flex-c-m size-108 how-pos1 bor0 fs-16 cl10 bg0 hov-btn3 trans-04" href="images/product-detail-01.jpg">
-												<i class="fa fa-expand"></i>
-											</a>
-										</div>
-									</div>
+								<div class="wrap-pic-w pos-relative">
+									<img class="img_modal2" alt="IMG-PRODUCT">
 								</div>
 							</div>
 						</div>
@@ -31,16 +23,16 @@
 					
 					<div class="col-md-6 col-lg-5 p-b-30">
 						<div class="p-r-50 p-t-5 p-lr-0-lg">
-							<h4 class="mtext-105 cl2 js-name-detail p-b-14">
+							<h4 class="mtext-105 cl2 js-name-detail p-b-14 tensanpham_modal">
 								Lightweight Jacket
 							</h4>
 
-							<span class="mtext-106 cl2">
+							<span class="mtext-106 cl2 gia_modal">
 								$58.79
 							</span>
 
-							<p class="stext-102 cl3 p-t-23">
-								Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.
+							<p class="stext-102 cl3 p-t-23 chitiet_sanpham_modal">
+							Simple design but lines. 100% elastic cotton 4-dimensional cotton fabric. Soft, smooth, cool sweat absorbent, Laundry washing machine are comfortable.
 							</p>
 							
 							<!--  -->
@@ -53,7 +45,6 @@
 									<div class="size-204 respon6-next">
 										<div class="rs1-select2 bor8 bg0">
 											<select class="js-select2" name="time">
-												<option>Choose an option</option>
 												<option>Size S</option>
 												<option>Size M</option>
 												<option>Size L</option>
@@ -72,7 +63,6 @@
 									<div class="size-204 respon6-next">
 										<div class="rs1-select2 bor8 bg0">
 											<select class="js-select2" name="time">
-												<option>Choose an option</option>
 												<option>Red</option>
 												<option>Blue</option>
 												<option>White</option>
@@ -96,21 +86,19 @@
 												<i class="fs-16 zmdi zmdi-plus"></i>
 											</div>
 										</div>
-
-										<button class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04 js-addcart-detail">
-											Add to cart
-										</button>
 									</div>
+									<div class="size-204 flex-w flex-m respon6-next">
+									<button  class="flex-c-m stext-101 cl0 bg1 bor1 hov-btn1 p-lr-15 trans-04 js-addcart-detail" style="margin-top:10px;width:140px;height: 46px;">
+										Add to cart
+									</button>
+									</div>
+									
 								</div>	
 							</div>
 
 							<!--  -->
 							<div class="flex-w flex-m p-l-100 p-t-40 respon7">
-								<div class="flex-m bor9 p-r-10 m-r-11">
-									<a href="#" class="fs-14 cl3 hov-cl1 trans-04 lh-10 p-lr-5 p-tb-2 js-addwish-detail tooltip100" data-tooltip="Add to Wishlist">
-										<i class="zmdi zmdi-favorite"></i>
-									</a>
-								</div>
+								
 
 								<a href="#" class="fs-14 cl3 hov-cl1 trans-04 lh-10 p-lr-5 p-tb-2 m-r-8 tooltip100" data-tooltip="Facebook">
 									<i class="fa fa-facebook"></i>
